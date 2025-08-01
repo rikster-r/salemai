b3cb86cd3f8e82cb957d90edcd155a99a90a0342
rikster-r <seitdanial43@gmail.com>
2025-08-01 16:25:35 +0500

    Change font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,114 @@
-import { Geist, Geist_Mono } from 'next/font/google';
 import Header from '@/components/Header';
+import localFont from 'next/font/local';
 import Head from 'next/head';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const firsNeue = localFont({
+  src: [
+    {
+      path: '../assets/fonts/TTFirsNeue-Thin.woff2',
+      weight: '100',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-ThinItalic.woff2',
+      weight: '100',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-ExtraLightItalic.woff2',
+      weight: '200',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Light.woff2',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-LightItalic.woff2',
+      weight: '300',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Italic.woff2',
+      weight: '400',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Medium.woff2',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-DemiBold.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-DemiBoldItalic.woff2',
+      weight: '600',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Bold.woff2',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-ExtraBoldItalic.woff2',
+      weight: '800',
+      style: 'italic'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-Black.woff2',
+      weight: '900',
+      style: 'normal'
+    },
+    {
+      path: '../assets/fonts/TTFirsNeue-BlackItalic.woff2',
+      weight: '900',
+      style: 'italic'
+    }
+  ],
+  display: 'swap',
+  variable: '--font-firs-neue',
+  fallback: ['system-ui', 'arial', 'sans-serif'],
+  preload: true,
+  adjustFontFallback: false
+})
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>
-          SalemAI
-        </title>
+        <title>SalemAI</title>
       </Head>
-      <div
-        className={`${geistSans.className} ${geistMono.className} font-sans flex justify-center min-h-screen`}
-      >
+      <div className={`font-sans flex justify-center min-h-screen ${firsNeue.className}`}>
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-[1000px] w-full">
           <Header />
         </main>
